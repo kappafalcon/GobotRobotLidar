9d7212365979af987cca7837e84a40c3409b0218
jsantore <jsantore@bridgew.edu>
2020-10-01 14:57:53 -0400

    first program to read from lidar and write to lcd

--- LidarRobot.go
+++ LidarRobot.go
@@ -1,9 +1,39 @@
 package main
 
-func robotMainLoop()  {
-	
+import (
+	"fmt"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/i2c"
+	g "gobot.io/x/gobot/platforms/dexter/gopigo3"
+	"gobot.io/x/gobot/platforms/raspi"
+	"time"
+)
+
+func robotMainLoop(piProcessor *raspi.Adaptor, gopigo3 *g.Driver, lidarSensor *i2c.LIDARLiteDriver,
+	lcd *i2c.GroveLcdDriver) {
+	lidarReading, err := lidarSensor.Distance()
+	if err != nil {
+		fmt.Println("Error reading lidar sensor %+v", err)
+	}
+	message := fmt.Sprintf("Lidar Reading: %d", lidarReading)
+	lcd.Write(message)
+	time.Sleep(time.Second * 3)
+
 }
 
-func main(){
-	
+func main() {
+	raspberryPi := raspi.NewAdaptor()
+	gopigo3 := g.NewDriver(raspberryPi)
+	lidarSensor := i2c.NewLIDARLiteDriver(raspberryPi)
+	lcd := i2c.NewGroveLcdDriver(raspberryPi)
+	workerThread := func() {
+		robotMainLoop(raspberryPi, gopigo3, lidarSensor, lcd)
+	}
+	robot := gobot.NewRobot("Gopigo Pi4 Bot",
+		[]gobot.Connection{raspberryPi},
+		[]gobot.Device{gopigo3, lidarSensor},
+		workerThread)
+
+	robot.Start()
+
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
-module "GobotRobotLidar"
+module GobotRobotLidar
+
+go 1.15
+
+require gobot.io/x/gobot v1.14.0
