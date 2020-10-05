f5f90a53802b9ee504e7c73b9eb01314defdabf3
jsantore <jsantore@bridgew.edu>
2020-10-05 13:27:26 -0400

    remove all commented lcd code

--- LidarRobot.go
+++ LidarRobot.go
@@ -12,37 +12,20 @@ import (
 
 func robotMainLoop(piProcessor *raspi.Adaptor, gopigo3 *g.Driver, lidarSensor *i2c.LIDARLiteDriver,
 
-/*lcd *i2c.GroveLcdDriver*/) {
-	//err := lcd.Start()
-	//if err != nil {
-	//	fmt.Println("error starting lcd")
-	//}
+) {
+
 	err := lidarSensor.Start()
 	if err != nil {
 		fmt.Println("error starting lidarSensor")
 	}
-	//err = lcd.SetRGB(100, 0, 0)
-	//if err != nil {
-	//	fmt.Println("Error setting lcd color")
-	//}
+
 	for { //loop forever
 		lidarReading, err := lidarSensor.Distance()
 		if err != nil {
 			fmt.Println("Error reading lidar sensor %+v", err)
 		}
 		message := fmt.Sprintf("Lidar Reading: %d", lidarReading)
-		//err = lcd.Clear()
-		//if err != nil {
-		//	fmt.Println("error clearing lcd")
-		//}
-		//err = lcd.Home()
-		//if err != nil {
-		//	fmt.Println("error homeing lcd")
-		//}
-		//err = lcd.Write(message)
-		//if err != nil {
-		//	fmt.Println("error writing to LCD")
-		//}
+
 		fmt.Println(lidarReading)
 		fmt.Println(message)
 		time.Sleep(time.Second * 3)
@@ -53,14 +36,13 @@ func main() {
 	raspberryPi := raspi.NewAdaptor()
 	gopigo3 := g.NewDriver(raspberryPi)
 	lidarSensor := i2c.NewLIDARLiteDriver(raspberryPi)
-	//lcd := i2c.NewGroveLcdDriver(raspberryPi)
 	lightSensor := aio.NewGroveLightSensorDriver(gopigo3, "AD_2_1")
 	workerThread := func() {
-		robotMainLoop(raspberryPi, gopigo3, lidarSensor /*lcd*/)
+		robotMainLoop(raspberryPi, gopigo3, lidarSensor)
 	}
 	robot := gobot.NewRobot("Gopigo Pi4 Bot",
 		[]gobot.Connection{raspberryPi},
-		[]gobot.Device{gopigo3, lidarSensor /*lcd,*/, lightSensor},
+		[]gobot.Device{gopigo3, lidarSensor, lightSensor},
 		workerThread)
 
 	robot.Start()
