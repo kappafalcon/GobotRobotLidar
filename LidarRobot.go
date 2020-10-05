a35ef8c0c56e302795cdaad6d2adceac0093aa5e
jsantore <jsantore@bridgew.edu>
2020-10-05 12:47:07 -0400

    comment out LCD functions

--- LidarRobot.go
+++ LidarRobot.go
@@ -10,37 +10,38 @@ import (
 )
 
 func robotMainLoop(piProcessor *raspi.Adaptor, gopigo3 *g.Driver, lidarSensor *i2c.LIDARLiteDriver,
-	lcd *i2c.GroveLcdDriver) {
-	err := lcd.Start()
-	if err != nil {
-		fmt.Println("error starting lcd")
-	}
-	err = lidarSensor.Start()
+
+/*lcd *i2c.GroveLcdDriver*/) {
+	//err := lcd.Start()
+	//if err != nil {
+	//	fmt.Println("error starting lcd")
+	//}
+	err := lidarSensor.Start()
 	if err != nil {
 		fmt.Println("error starting lidarSensor")
 	}
-	err = lcd.SetRGB(100, 0, 0)
-	if err != nil {
-		fmt.Println("Error setting lcd color")
-	}
+	//err = lcd.SetRGB(100, 0, 0)
+	//if err != nil {
+	//	fmt.Println("Error setting lcd color")
+	//}
 	for { //loop forever
 		lidarReading, err := lidarSensor.Distance()
 		if err != nil {
 			fmt.Println("Error reading lidar sensor %+v", err)
 		}
 		message := fmt.Sprintf("Lidar Reading: %d", lidarReading)
-		err = lcd.Clear()
-		if err != nil {
-			fmt.Println("error clearing lcd")
-		}
-		err = lcd.Home()
-		if err != nil {
-			fmt.Println("error homeing lcd")
-		}
-		err = lcd.Write(message)
-		if err != nil {
-			fmt.Println("error writing to LCD")
-		}
+		//err = lcd.Clear()
+		//if err != nil {
+		//	fmt.Println("error clearing lcd")
+		//}
+		//err = lcd.Home()
+		//if err != nil {
+		//	fmt.Println("error homeing lcd")
+		//}
+		//err = lcd.Write(message)
+		//if err != nil {
+		//	fmt.Println("error writing to LCD")
+		//}
 		fmt.Println(lidarReading)
 		fmt.Println(message)
 		time.Sleep(time.Second * 3)
@@ -53,7 +54,7 @@ func main() {
 	lidarSensor := i2c.NewLIDARLiteDriver(raspberryPi)
 	lcd := i2c.NewGroveLcdDriver(raspberryPi)
 	workerThread := func() {
-		robotMainLoop(raspberryPi, gopigo3, lidarSensor, lcd)
+		robotMainLoop(raspberryPi, gopigo3, lidarSensor /*lcd*/)
 	}
 	robot := gobot.NewRobot("Gopigo Pi4 Bot",
 		[]gobot.Connection{raspberryPi},
